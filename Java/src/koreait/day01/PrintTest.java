1827b1b5f37926b8036dcaa15ce34e58191346f4
kimdonghun <kdh462601@gmail.com>
2022-01-28 13:39:16 +0900

    2021_12_28

---
+++ Java/src/koreait/day01/PrintTest.java
@@ -0,0 +1,41 @@
+package koreait.day01;	//koreait.day1은 패키지 이름
+
+public class PrintTest {	//PrintTest는 클래스 이름
+//설명:주석(comment)
+
+	public static void main(String[] args) {
+		// day1 첫번째 연습 : 출력하기
+		
+		System.out.println("Hello world");
+		//syso 입력후 Ctrl + 스페이스바로 자동완성 하세요.
+		System.out.println("----------------------");
+		System.out.println("End!");
+		System.out.println();
+		
+		short b_val;
+		int i_val = 87;
+		long l_val;
+		char ch1 = 's';
+		double d_val=7.345;
+		
+		l_val = 234;
+		b_val=23;
+		i_val = ch1;
+		d_val = i_val;
+//		i_val = d_val;
+		
+		
+	}
+
+}
+//저장 : Ctrl + S , 실행 : Ctrl + f11, 한줄 복사 : Ctrl+Alt+아래방향키
+/* 자바는 클래스를 이용하는 프로그래밍 방식이다.
+예) 구스 다운을 사고 싶다 -> K2 매장, 치킨을 먹고싶다 -> bhc
+커피를 마시고 싶다 -> 스타벅스
+
+ 클래스는 콘솔에 출력하고 싶다 -> System클래스 사용(출력과 입력기능)\
+ 주어진 문장에서 korea 단어 검색하고 싶다. args -> String클래스 사용(찾기 기능)
+ 
+ 현실에서 사용할 프로그램은 요구사항 -> 프로그램 개발 -> 적절한 java 클래스를 사용
+ */
+
---
+++ Java/src/koreait/day01/TwicePrint.java
@@ -0,0 +1,20 @@
+package koreait.day01;
+
+public class TwicePrint {
+
+	public static void main(String[] args) {
+		System.out.println("     Twice!     ");
+		System.out.println("------------------");
+		System.out.println("나연	1995.09.22");
+		System.out.println("정연	1996.11.01");
+		System.out.println("사나	1996.12.29");
+		System.out.println("지효	1997.02.01");
+		System.out.println("미나	1997.03.24");
+		System.out.printf("%s\t%s","다현","1996.12.29\n");
+		System.out.printf("%s\t%s","채영","1996.12.29\n");
+		System.out.printf("%s\t%s","쯔위","1996.12.29\n");
+		System.out.printf("%s\t%s","모모","1996.11.09\n");
+	}
+
+	// \n, \t : 특정기능문자(이스케이프 문자)
+}
